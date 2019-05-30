68f318c97c81b57fac30b9abf8f813673790cbcf
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-05-30 20:31:58 +0400

    dop по js

--- src/js/main.js
+++ src/js/main.js
@@ -25,5 +25,36 @@ buttonCloseEL.addEventListener('click', function(){
 });    
 
 
+// let type1 = 'Petr',
+// 		type2 = 50,
+// 		type3 = true,
+// 		type4 = null,
+// 		type5 = undefined,
+// 		type6 = {},
+// 		type7 = function(){};
+		
+// console.log('тип данных "Petr" = ' + typeof(type1));
+// console.log('тип данных "50" = ' +typeof(type2));
+// console.log('тип данных "true" = ' +typeof(type3));
+// console.log('тип данных "null" = ' +typeof(type4));
+// console.log('тип данных "undefined" = ' +typeof(type5));
+// console.log('тип данных "{}" = ' +typeof(type6));
+// console.log('тип данных "function(){}" = ' +typeof(type7));
+
+
+let width  = 500,
+		height = 400;
+console.log('width = 500; height = 400; шаг итерации +10 к width');
+
+let calculateArea = function() {
+		for(let i = 0; i <=10 ; i++) {
+			let area = width * height;
+			console.log('Искомая площадь = ' + area);
+			width += 10;
+		}
+};
+
+calculateArea();
+
 
 })();
