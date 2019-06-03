aac3138e410c4bdfdfd172d33bbfed1ae731a55d
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-03 16:21:49 +0400

    подключили слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -30,8 +30,42 @@ arrowupEl.click(function() {
   return false;
  });
 
+});
 
+// == слайдер ==
 
+$(document).ready(function(){
+  $('.slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		// autoplay: true,
+		prevArrow: $('.slide__left-arrow'),
+		nextArrow: $('.slide__rigth-arrow'),
+		 responsive: [
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+    },
+  ]
+  });
 });
 
 
@@ -42,64 +76,3 @@ arrowupEl.click(function() {
 
 
 
-
-// 'use strict';
-
-// (function(){
-
-// let buttonEL      = document.querySelector('#btn'),
-// 	  modalEL       = document.querySelector('.modal'),
-//     buttonCloseEL = document.querySelector('#btn-close');
-
-// buttonEL.addEventListener('click', function() {
-// 	let timer = setTimeout (closeModal, 5000);
-// 	clearTimeout(timer);
-// 	modalEL.classList.add('modal_activ');
-// 	setTimeout (closeModal, 5000);
-	
-	
-// }); 
-
-// let closeModal = function() {
-// 		modalEL.classList.remove('modal_activ');
-// 	};
-
-	
-// buttonCloseEL.addEventListener('click', function() {
-// 	modalEL.classList.remove('modal_activ');
-// });    
-
-
-// let type1 = 'Petr',
-// 		type2 = 50,
-// 		type3 = true,
-// 		type4 = null,
-// 		type5 = undefined,
-// 		type6 = {},
-// 		type7 = function(){};
-		
-// console.log('тип данных "Petr" = ' + typeof(type1));
-// console.log('тип данных "50" = ' +typeof(type2));
-// console.log('тип данных "true" = ' +typeof(type3));
-// console.log('тип данных "null" = ' +typeof(type4));
-// console.log('тип данных "undefined" = ' +typeof(type5));
-// console.log('тип данных "{}" = ' +typeof(type6));
-// console.log('тип данных "function(){}" = ' +typeof(type7));
-
-
-// let width  = 500,
-// 		height = 400;
-// console.log('width = 500; height = 400; шаг итерации +10 к width');
-
-// let calculateArea = function() {
-// 		for(let i = 0; i <=10 ; i++) {
-// 			let area = width * height;
-// 			console.log('Искомая площадь = ' + area);
-// 			width += 10;
-// 		}
-// };
-
-// calculateArea();
-
-
-// })();
